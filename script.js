2a77b4c120856641d0fed6143e8a868769b0f48e
Studentabel <abelmorad97@gmail.com>
2023-02-22 20:42:08 +0800

    Update now with hamburger menu active

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-// readmore bg color change at active&inactive
+// readmore bg color change at active & inactive
 const readMore = document.getElementById('readMore');
 readMore.addEventListener('mousedown', activeBtn);
 readMore.addEventListener('mouseup', inactiveBtn);
 
 function activeBtn () {
-    readMore.style.backgroundColor = "hsl(240, 100%, 5%)";
+    readMore.style.backgroundColor = 'hsl(240, 100%, 5%)';
 }
 function inactiveBtn () {
-    readMore.style.backgroundColor = "hsl(5, 85%, 63%)";
+    readMore.style.backgroundColor = 'hsl(5, 85%, 63%)';
+}
+
+// hamburger button open & close the nav menu
+const hamburgerBtn = document.getElementById('hamburgerBtn');
+const hamburgerBtnX = document.getElementById('hamburgerBtnX');
+const hamburgerMenu = document.getElementById('hamburgerMenu');
+const hamburgerMenuActive = document.querySelector('.nav-links-active');
+
+hamburgerBtn.addEventListener('click', menuBtnActive);
+hamburgerBtnX.addEventListener('click', menuBtnInactive);
+
+function menuBtnActive () {
+    hamburgerMenu.classList.toggle('nav-links-active');
+}
+function menuBtnInactive () {
+    hamburgerMenu.classList.remove('nav-links-active');
 }
